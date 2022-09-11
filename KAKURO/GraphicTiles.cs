b5803df13433530b5988fd66ff75b74d2e051702
Denis Mitlenko <softonka@gmail.com>
2022-09-11 17:40:19 +0300

    Started to rework GraphicTiles

--- KAKURO/GraphicTiles.cs
+++ KAKURO/GraphicTiles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,14 +27,21 @@ namespace KAKURO
 
     internal class GraphicTile
     {
-        public PictureBox Picture { get; set; }
+        public PictureBox Canvas { get; set; }
         public Bitmap buffer { get; set; }
         public TileType Type { get; }
 
-        public GraphicTile(TileType type) { Picture = null; Type = type; }
-        public GraphicTile(PictureBox pb, TileType type) { Picture = pb; Type = type; }
-        
-        public GraphicTile(PictureBox pb) { Picture = pb; Type = null; }
+        public Point Position { get; set; }
+        public Size Size { get; set; }
+
+        public bool Selected { get; set; }
+
+        public GraphicTile(TileType type) { Canvas = null; Type = type; Selected = false; Position = Point.Empty; Size = Size.Empty; }
+        public GraphicTile(PictureBox pb, TileType type) { Canvas = pb; Type = type; Selected = false; Position = Point.Empty; Size = Size.Empty; }
+
+        public GraphicTile(PictureBox pb) { Canvas = pb; Type = null; Selected = false; Position = Point.Empty; Size = Size.Empty; }
+
+
 
         public virtual void Draw() { }
     }
@@ -44,18 +52,25 @@ namespace KAKURO
         public BlackGraphicTile() : base(TileTypes.Black) { }
 
         public override void Draw() {
-            buffer = new Bitmap(Picture.Width, Picture.Height);
+            buffer = new Bitmap(Canvas.Image);
 
             Task.Factory.StartNew(() =>
             {
                 using (Graphics g = Graphics.FromImage(buffer))
                 {
-                    g.Clear(Color.Black);
+                    g.DrawRectangle(new Pen(Brushes.Black), new Rectangle(Position, Size));
+
+                    if (Selected)
+                    {
+                        Pen pen = new Pen(Color.White, 2);
+                        pen.Alignment = PenAlignment.Inset;
+                        g.DrawRectangle(pen, new Rectangle(Position, Size));
+                    }
                 }
 
-                Picture.Invoke(new Action(() =>
+                Canvas.Invoke(new Action(() =>
                 {
-                    Picture.Image = buffer;
+                    Canvas.Image = buffer;
                 }));
             });
         }
@@ -94,26 +109,33 @@ namespace KAKURO
 
         public override void Draw()
         {
-            buffer = new Bitmap(Picture.Width, (Picture.Height));
+            buffer = new Bitmap(Canvas.Image);
 
             Task.Factory.StartNew(() =>
             {
                 using (Graphics g = Graphics.FromImage(buffer))
                 {
                     g.Clear(Color.Black);
-                    g.DrawLine(new Pen(Color.White, 2), new Point(-2, -2), new Point(Picture.Width + 2, Picture.Height + 2));
+                    g.DrawLine(new Pen(Color.White, 2), new Point(-2, -2), new Point(Canvas.Width + 2, Canvas.Height + 2));
 
                     int padding = 2;
                     int fontSize = 16;
                     Font drawFont = new Font(FontFamily.GenericMonospace, fontSize, FontStyle.Bold, GraphicsUnit.Pixel);
 
-                    g.DrawString(SumLeft == 0 ? "" : SumLeft.ToString(), drawFont, Brushes.White, padding, Picture.Height - GetStringWidth(SumRight.ToString(), drawFont).Item2);
-                    g.DrawString(SumRight == 0 ? "" : SumRight.ToString(), drawFont, Brushes.White, Picture.Width - GetStringWidth(SumRight.ToString(), drawFont).Item1, padding);
+                    g.DrawString(SumLeft == 0 ? "" : SumLeft.ToString(), drawFont, Brushes.White, padding, Canvas.Height - GetStringWidth(SumRight.ToString(), drawFont).Item2);
+                    g.DrawString(SumRight == 0 ? "" : SumRight.ToString(), drawFont, Brushes.White, Canvas.Width - GetStringWidth(SumRight.ToString(), drawFont).Item1, padding);
+
+                    if (Selected)
+                    {
+                        Pen pen = new Pen(Color.White, 2);
+                        pen.Alignment = PenAlignment.Inset;
+                        g.DrawRectangle(pen, new Rectangle(Position, Size));
+                    }
                 }
 
-                Picture.Invoke(new Action(() =>
+                Canvas.Invoke(new Action(() =>
                 {
-                    Picture.Image = buffer;
+                    Canvas.Image = buffer;
                 }));
             });
         }
@@ -137,24 +159,31 @@ namespace KAKURO
 
         public override void Draw()
         {
-            buffer = new Bitmap(Picture.Width, Picture.Height);
+            buffer = new Bitmap(Canvas.Image);
 
             Task.Factory.StartNew(() =>
             {
                 using (Graphics g = Graphics.FromImage(buffer))
                 {
                     g.Clear(Color.Black);
-                    g.FillRectangle(Brushes.White, new Rectangle(3, 3, Picture.Width - 3, Picture.Height - 3));
+                    g.FillRectangle(Brushes.White, new Rectangle(3, 3, Canvas.Width - 3, Canvas.Height - 3));
 
                     int fontSize = 48;
                     Font drawFont = new Font(FontFamily.GenericMonospace, fontSize, FontStyle.Bold, GraphicsUnit.Pixel);
 
                     g.DrawString(DrawnNumber == 0 ? "" : DrawnNumber.ToString(), drawFont, Brushes.Blue, 0, 0);
+
+                    if (Selected)
+                    {
+                        Pen pen = new Pen(Color.White, 2);
+                        pen.Alignment = PenAlignment.Inset;
+                        g.DrawRectangle(pen, new Rectangle(Position, Size));
+                    }
                 }
 
-                Picture.Invoke(new Action(() =>
+                Canvas.Invoke(new Action(() =>
                 {
-                    Picture.Image = buffer;
+                    Canvas.Image = buffer;
                 }));
             });
         }

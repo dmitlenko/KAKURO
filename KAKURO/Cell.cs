1d2c67dfb8b545490933ab013ca43110ce2fe2d0
Denis Mitlenko <softonka@gmail.com>
2022-09-13 14:06:51 +0300

    Made Cell class to separate Generator and TileController

---
+++ KAKURO/Cell.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAKURO
+{
+    [Serializable]
+    public class Cell
+    {
+        public string Type { get; }
+
+        public Cell() => Type = "cell";
+
+        public Cell(string type) => Type = type;
+    }
+
+    [Serializable]
+    public class BlackCell : Cell
+    {
+        public BlackCell(): base("black") { }
+    }
+
+    [Serializable]
+    public class HintCell : Cell
+    {
+        public int HorizontalSum { get; } = 0;
+        public int VerticalSum { get; } = 0;
+
+        public HintCell(): base("hint") { }
+        public HintCell(int verticalSum,int horizontalSum): base("hint")
+        {
+            HorizontalSum = horizontalSum;
+            VerticalSum = verticalSum;
+        }
+    }
+
+    [Serializable]
+    public class NumberCell: Cell
+    {
+        public int Number { get; set; } = 0;
+
+        public NumberCell(): base("number") { }
+        public NumberCell(int number): base("number") => Number = number;
+    }
+}
--- KAKURO/TileController.cs
+++ KAKURO/TileController.cs
@@ -12,7 +12,7 @@ namespace KAKURO
     {
         private bool _enabled = true;
 
-        public PictureBox Canvas { get; }
+        public PictureBox Canvas { get; set; }
         public GraphicTile[,] GraphicTiles;
 
         public Point Selected = new Point(0,0);
@@ -49,24 +49,35 @@ namespace KAKURO
             }
         }
 
-        public TileController(PictureBox canvas, int tilesX, int tilesY, GraphicTile[,] tiles)
+        public TileController(PictureBox canvas) 
         {
-            GraphicTiles = new GraphicTile[tilesY, tilesX];
+            Canvas = canvas;
+            Enabled = false;
+
+            PrepareCanvas();
+        }
+
+        public TileController(PictureBox canvas, int tilesX, int tilesY) 
+        {
+            Canvas = canvas;
+            Size = new Size(tilesX, tilesY);
+            Enabled = false;
+
+            PrepareCanvas();
+        }
+
+        public TileController(PictureBox canvas, int tilesX, int tilesY, Cell[,] cells)
+        {
             Canvas = canvas;
             Size = new Size(tilesX, tilesY);
             Enabled = true;
 
-            PrepareBoxTiles();
-
-            for (int i = 0; i < tiles.GetLength(0); i++)
-                for (int j = 0; j < tiles.GetLength(1); j++)
-                    GraphicTiles[i, j] = tiles[i, j];
+            AssignCells(cells);
 
             PrepareCanvas();
             Update();
         }
 
-        // Canvas click
         private void Canvas_MouseDown(object sender, MouseEventArgs e)
         {
             if (Enabled)
@@ -79,18 +90,6 @@ namespace KAKURO
                     Update();
                 }
             }
-        }
-
-        private void PrepareBoxTiles()
-        {
-            int tileHW = Canvas.Height / Size.Height;
-            for (int i = 0; i < GraphicTiles.GetLength(0); i++)
-                for(int j = 0; j < GraphicTiles.GetLength(1); j++)
-                {
-                    GraphicTiles[i, j] = new BlackGraphicTile(Canvas);
-                    GraphicTiles[i, j].Position = new Point(i * tileHW, j * tileHW);
-                    GraphicTiles[i, j].Size = new Size(tileHW, tileHW);
-                }
         }
 
         private void PrepareCanvas()
@@ -110,7 +109,7 @@ namespace KAKURO
         private Point TopHintFromSelection()
         {
             for (int i = Selected.Y; i >= 0; i--)
-                if (GraphicTiles[i, Selected.X].Type.ToString() == "hint")
+                if (GraphicTiles[i, Selected.X].Type == TileTypes.Hint)
                     return new Point(Selected.X, i);
 
             return Point.Empty;
@@ -119,7 +118,7 @@ namespace KAKURO
         private Point LeftHintFromSelection()
         {
             for (int i = Selected.X; i >= 0; i--)
-                if (GraphicTiles[Selected.Y, i].Type.ToString() == "hint")
+                if (GraphicTiles[Selected.Y, i].Type == TileTypes.Hint)
                     return new Point(i, Selected.Y);
 
             return Point.Empty;
@@ -132,9 +131,9 @@ namespace KAKURO
 
             for (int i = x + 1; i < GraphicTiles.GetLength(1); i++)
             {
-                if (GraphicTiles[i, x].Type.ToString() == "black") break;
+                if (GraphicTiles[i, x].Type == TileTypes.Black) break;
 
-                if (GraphicTiles[y, i].Type.ToString() == "number")
+                if (GraphicTiles[y, i].Type == TileTypes.Number)
                 {
                     num = ((NumberGraphicTile)GraphicTiles[y, i]).DrawnNumber;
                     sum += num;
@@ -151,9 +150,9 @@ namespace KAKURO
 
             for (int i = y + 1; i < GraphicTiles.GetLength(0); i++)
             {
-                if (GraphicTiles[i, x].Type.ToString() == "black") break;
+                if (GraphicTiles[i, x].Type == TileTypes.Black) break;
 
-                if (GraphicTiles[i, x].Type.ToString() == "number")
+                if (GraphicTiles[i, x].Type == TileTypes.Number)
                 {
                     num = ((NumberGraphicTile)GraphicTiles[i, x]).DrawnNumber;
                     sum += num;
@@ -165,7 +164,41 @@ namespace KAKURO
 
         public Box this[int x, int y] { get => new Box(ref GraphicTiles[y, x]); }
 
-        public void AssignTiles(GraphicTile[,] tiles) =>  GraphicTiles = tiles;
+        public void AssignCells(Cell[,] cells)
+        {
+            GraphicTile[,] tiles = new GraphicTile[Size.Height, Size.Width];
+
+            for (int i = 0; i < Size.Height; i++)
+                for (int j = 0; j < Size.Height; j++)
+                    tiles[i, j] = new BlackGraphicTile();
+
+            for (int i = 0; i < cells.GetLength(0); i++)
+                for (int j = 0; j < cells.GetLength(1); j++)
+                    if (cells[i, j].Type == "black" || cells[i, j].Type == "cell")
+                        tiles[i, j] = new BlackGraphicTile();
+                    else if (cells[i, j].Type == "hint")
+                        tiles[i, j] = new HintGraphicTile(((HintCell)cells[i, j]).VerticalSum, ((HintCell)cells[i, j]).HorizontalSum);
+                    else
+                        tiles[i, j] = new NumberGraphicTile(((NumberCell)cells[i, j]).Number);
+
+            GraphicTiles = tiles;
+        }
+
+        public Cell[,] CellData()
+        {
+            Cell[,] cells = new Cell[Size.Height, Size.Width];
+
+            for (int i = 0; i < Size.Height; i++)
+                for (int j = 0; j < Size.Width; j++)
+                    if (GraphicTiles[i, j].Type == TileTypes.Black || GraphicTiles[i, j].Type == TileTypes.Empty)
+                        cells[i, j] = new Cell();
+                    else if (GraphicTiles[i, j].Type == TileTypes.Hint)
+                        cells[i, j] = new HintCell(((HintGraphicTile)GraphicTiles[i, j]).SumVertical, ((HintGraphicTile)GraphicTiles[i, j]).SumHorizontal);
+                    else
+                        cells[i, j] = new NumberCell(((NumberGraphicTile)GraphicTiles[i, j]).DrawnNumber);
+
+            return cells;
+        }
 
         public void MoveSelectionUp()
         {
@@ -211,7 +244,7 @@ namespace KAKURO
         {
             if (Enabled)
             {
-                if (GraphicTiles[Selected.Y, Selected.X].Type.ToString() == "number")
+                if (GraphicTiles[Selected.Y, Selected.X].Type == TileTypes.Number)
                     ((NumberGraphicTile)GraphicTiles[Selected.Y, Selected.X]).DrawnNumber = number;
 
                 Update();
@@ -220,6 +253,8 @@ namespace KAKURO
 
         public void Update()
         {
+            if (Size.Width <= 0 && Size.Height <= 0) return;
+
             Bitmap buffer = new Bitmap(Canvas.Width + 1, Canvas.Height + 1);
 
             Task.Factory.StartNew(() =>
@@ -227,6 +262,8 @@ namespace KAKURO
                 int tileHW = Canvas.Width / Size.Width;
                 using (Graphics g = Graphics.FromImage(buffer))
                 {
+                    if (GraphicTiles == null) return;
+
                     for (int i = 0; i < GraphicTiles.GetLength(0); i++)
                     {
                         for (int j = 0; j < GraphicTiles.GetLength(1); j++)
@@ -235,15 +272,15 @@ namespace KAKURO
                             GraphicTiles[i, j].Position = new Point(j * tileHW, i * tileHW);
                             GraphicTiles[i, j].Selected = Selected.X == j && Selected.Y == i;
 
-                            if (GraphicTiles[i, j].Type.ToString() == "hint")
+                            if (GraphicTiles[i, j].Type == TileTypes.Hint)
                             {
                                 ((HintGraphicTile)GraphicTiles[i, j]).HighlightVertical = false;
                                 ((HintGraphicTile)GraphicTiles[i, j]).HighlightHorizontal = false;
 
                                 if (HighlightWrongSums)
                                 {
-                                    int sumH = ((HintGraphicTile)GraphicTiles[i,j]).SumHorizontal;
-                                    int sumV = ((HintGraphicTile)GraphicTiles[i,j]).SumVertical;
+                                    int sumH = ((HintGraphicTile)GraphicTiles[i, j]).SumHorizontal;
+                                    int sumV = ((HintGraphicTile)GraphicTiles[i, j]).SumVertical;
 
                                     int sumH1 = HorizontalSum(j, i);
                                     int sumV1 = VerticalSum(j, i);
@@ -262,7 +299,7 @@ namespace KAKURO
                         Point th = TopHintFromSelection();
                         Point lh = LeftHintFromSelection();
 
-                        if(GraphicTiles[th.Y, th.X].Type.ToString() == "hint" &&  GraphicTiles[lh.Y, lh.X].Type.ToString() == "hint" && Selected != th && Selected != lh)
+                        if (GraphicTiles[th.Y, th.X].Type == TileTypes.Hint && GraphicTiles[lh.Y, lh.X].Type == TileTypes.Hint && Selected != th && Selected != lh)
                         {
                             ((HintGraphicTile)GraphicTiles[th.Y, th.X]).HighlightVertical = true;
                             ((HintGraphicTile)GraphicTiles[lh.Y, lh.X]).HighlightHorizontal = true;
@@ -272,8 +309,8 @@ namespace KAKURO
                         }
                     }
 
-                    if(!Enabled)
-                        g.FillRectangle(new SolidBrush(Color.FromArgb(0x70ffffff)), new Rectangle(0, 0, Canvas.Width, Canvas.Height));
+                    if (!Enabled)
+                        g.FillRectangle(new SolidBrush(Color.FromArgb(0xd0,Color.White)), new Rectangle(0, 0, Canvas.Width, Canvas.Height));
                 }
 
                 Canvas.Invoke(new Action(() => {

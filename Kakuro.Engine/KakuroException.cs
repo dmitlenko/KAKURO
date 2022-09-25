2d85b61d964f75bca994f6d68ce10dfb0aeab8e9
Denis Mitlenko <softonka@gmail.com>
2022-09-25 21:28:20 +0300

    Custom exception

---
+++ Kakuro.Engine/KakuroException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakuro.Engine
+{
+    /**
+     * <summary>Class for kakuro exceptions</summary>
+     */
+    public class KakuroException: Exception
+    {
+        /**
+         * <summary>Error message</summary>
+         */
+        public string ErrorMessage;
+
+        /**
+         * <summary>Default constructor for KakuroException</summary>
+         */
+        public KakuroException(): base() { }
+
+        /**
+         * <summary>Constructor for KakuroException</summary>
+         * <param name="message">Error message</param>
+         */
+        public KakuroException(string message): base(message) { 
+            ErrorMessage = message;
+        }
+    }
+}

5c6771eb9644835773f9fcbf58e3e482663f9b62
Denis Mitlenko <softonka@gmail.com>
2022-09-25 21:27:00 +0300

    Created Algorithms namespace for all algorithms

---
+++ Kakuro.Engine/Algorithms/Combos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kakuro.Engine.Algorithms
+{
+    /**
+     * <summary>Class to represent all possible combinations of numbers in a given amount of white squares.</summary>
+     */
+    public class Combos
+    {
+    }
+}
---
+++ Kakuro.Engine/Algorithms/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Kakuro.Engine.Algorithms
+{
+    public class Generator
+    {
+        private Kakuro k;
+        private int[] candidate_cells;
+        private Dictionary<int, bool[]> cell_validity;
+        private Dictionary<int, int> cell_index;
+
+        /**
+         * <summary>Default constructor for Generator</summary>
+         */
+        public Generator()
+        {
+            k = null;
+            candidate_cells = null;
+            cell_validity = null;
+            cell_index = null;
+        }
+
+        /**
+         * <summary>Generate valid kakuro board</summary>
+         * <param name="width">Board width</param>
+         * <param name="height">Board height</param>
+         * <param name="difficulty">Board difficulty</param>
+         * <returns>Returns kakuro board</returns>
+         */
+        public Kakuro Generate(int width, int height, int difficulty)
+        {
+            k = new Kakuro(width, height);
+            k.Difficulty = difficulty;
+        }
+    }
+}

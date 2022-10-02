6022a3511108669d968bdc2290c7c019419a0e0c
Denis Mitlenko <softonka@gmail.com>
2022-10-03 00:25:55 +0300

    Added some misc function in Authenticator

--- Kakuro.Engine/Authorization/Authenticator.cs
+++ Kakuro.Engine/Authorization/Authenticator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Kakuro.Engine.Authorization
@@ -61,6 +62,26 @@ namespace Kakuro.Engine.Authorization
         {
             User u = new User(displayName, username, Hasher.Hash(password), userData.NewUID);
             return userData.Add(u, true);
+        }
+
+        /**
+         * <summary>Checks if inputed password is valid</summary>
+         * <param name="password">Password</param>
+         * <returns>Is password valid?</returns>
+         */
+        public bool CheckPassword(string password)
+        {
+            return !string.IsNullOrWhiteSpace(password) && !password.Contains(' ') && password.Length >= 8 && password.Length <= 32;
+        }
+
+        /**
+         * <summary>Checks if inputed username is valid</summary>
+         * <param name="username">Username</param>
+         * <returns>Is username valid?</returns>
+         */
+        public bool CheckUsername(string username)
+        {
+            return CheckPassword(username) && Regex.IsMatch(username, "^[a-zA-Z0-9_]*$");
         }
     }
 }

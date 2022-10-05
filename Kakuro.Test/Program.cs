f390fd8a38159e6306eedb41a8a59fb74a7ec092
Denis Mitlenko <softonka@gmail.com>
2022-10-05 19:53:34 +0300

    Added test program for debuging purposes

---
+++ Kakuro.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Kakuro.Engine.Algorithms;
+using Kakuro.Engine.Core;
+
+class Program
+{
+    public static void Main()
+    {
+        Generator generator = new Generator();
+
+        try
+        {
+            KakuroBoard board = generator.Generate(8, 8, 3);
+
+        } catch(KakuroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}

b2564c98f7304cda1b4c09b9815f6d5a02f9677d
Denis Mitlenko <softonka@gmail.com>
2022-10-11 20:42:18 +0300

    Added unit tests

---
+++ Kakuro.Test/GeneratorTest.cs
@@ -0,0 +1,128 @@
+﻿using Kakuro.Engine;
+using Kakuro.Engine.Algorithms;
+using Kakuro.Engine.Cells;
+using Kakuro.Engine.Core;
+
+
+namespace Kakuro.Test;
+
+[TestClass]
+public class GeneratorTest
+{
+    [TestMethod]
+    public void Test4x4Easy()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(4, 4, 1);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 4);
+        Assert.AreEqual(b.Height, 4);
+        Assert.AreEqual(b.Difficulty, 1);
+    }
+
+    [TestMethod]
+    public void Test4x4Medium()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(4, 4, 2);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 4);
+        Assert.AreEqual(b.Height, 4);
+        Assert.AreEqual(b.Difficulty, 2);
+    }
+
+    [TestMethod]
+    public void Test4x4Hard()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(4, 4, 3);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 4);
+        Assert.AreEqual(b.Height, 4);
+        Assert.AreEqual(b.Difficulty, 3);
+    }
+
+    [TestMethod]
+    public void Test8x8Easy()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(8, 8, 1);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 8);
+        Assert.AreEqual(b.Height, 8);
+        Assert.AreEqual(b.Difficulty, 1);
+    }
+
+    [TestMethod]
+    public void Test8x8Medium()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(8, 8, 2);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 8);
+        Assert.AreEqual(b.Height, 8);
+        Assert.AreEqual(b.Difficulty, 2);
+    }
+
+    [TestMethod]
+    public void Test8x8Hard()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(8, 8, 3);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 8);
+        Assert.AreEqual(b.Height, 8);
+        Assert.AreEqual(b.Difficulty, 3);
+    }
+
+    [TestMethod]
+    public void Test16x16Easy()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(16, 16, 1);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 16);
+        Assert.AreEqual(b.Height, 16);
+        Assert.AreEqual(b.Difficulty, 1);
+    }
+
+    [TestMethod]
+    public void Test16x16Medium()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(16, 16, 2);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 16);
+        Assert.AreEqual(b.Height, 16);
+        Assert.AreEqual(b.Difficulty, 2);
+    }
+
+    [TestMethod]
+    public void Test16x16Hard()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(16, 16, 3);
+
+        Assert.IsTrue(s.Validate(b));
+        Assert.AreEqual(b.Width, 16);
+        Assert.AreEqual(b.Height, 16);
+        Assert.AreEqual(b.Difficulty, 3);
+    }
+}
---
+++ Kakuro.Test/SolverTest.cs
@@ -0,0 +1,65 @@
+using Kakuro.Engine.Algorithms;
+using Kakuro.Engine.Cells;
+using Kakuro.Engine.Core;
+
+namespace Kakuro.Test;
+
+[TestClass]
+public class SolverTest
+{
+    Cell[,] grid5x5 = new Cell[,]
+    {
+        {new BlackCell(), new BlackCell(), new BlackCell(), new SumCell(-1,24), new SumCell(-1,7)},
+        {new BlackCell(), new SumCell(-1,6), new SumCell(8,24), new WhiteCell(), new WhiteCell()},
+        {new SumCell(18,-1), new WhiteCell(), new WhiteCell(), new WhiteCell(), new WhiteCell()},
+        {new SumCell(23,-1), new WhiteCell(), new WhiteCell(), new WhiteCell(), new WhiteCell()},
+        {new SumCell(12,-1), new WhiteCell(), new WhiteCell(), new BlackCell(), new BlackCell()},
+    };
+
+    Cell[,] grid3x3 = new Cell[,]
+    {
+        {new BlackCell(), new SumCell(-1,4), new SumCell(-1,5)},
+        {new SumCell(4,-1), new WhiteCell(), new WhiteCell()},
+        {new SumCell(5,-1), new WhiteCell(), new WhiteCell()}
+    };
+
+    Cell[,] grid13x13 = new Cell[,]
+    {
+        {new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(-1,16),new SumCell(-1,3),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell()},
+        {new BlackCell(),new SumCell(-1,16),new SumCell(-1,21),new BlackCell(),new SumCell(11,-1),new WhiteCell(),new WhiteCell(),new SumCell(-1,28),new BlackCell(),new SumCell(-1,29),new SumCell(-1,17),new BlackCell(),new BlackCell()},
+        {new SumCell(4,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(12,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(14,7),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell() },
+        {new SumCell(7,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(-1,16),new SumCell(-1,4),new SumCell(26,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell()},
+        {new SumCell(3,-1),new WhiteCell(),new WhiteCell(),new SumCell(12,23),new WhiteCell(),new WhiteCell(),new SumCell(11,29),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell(),new BlackCell() },
+        {new BlackCell(),new SumCell(41,16),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,7),new SumCell(-1,38),new SumCell(-1,3)},
+        {new SumCell(17,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(15,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(6,-1),new WhiteCell(),new WhiteCell(),new WhiteCell() },
+        {new SumCell(21,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,29),new SumCell(8,23),new WhiteCell(),new WhiteCell(),new SumCell(-1,16),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(39,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,6)},
+        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(8,-1),new WhiteCell(),new WhiteCell(),new SumCell(6,-1),new WhiteCell(),new WhiteCell()},
+        {new BlackCell(),new BlackCell(),new SumCell(20,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,17),new SumCell(-1,16),new BlackCell(),new SumCell(9,-1),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new SumCell(12,-1),new WhiteCell(),new WhiteCell(),new SumCell(21,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(10,-1),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(17,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell() }
+    };
+
+    [TestMethod]
+    public void Test5x5()
+    {
+        Solver s = new Solver();
+        Assert.IsTrue(s.Validate(new KakuroBoard(grid5x5)));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(KakuroException))]
+    public void Test3x3()
+    {
+        Solver s = new Solver();
+        s.Validate(new KakuroBoard(grid3x3));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(KakuroException))]
+    public void Test13x13()
+    {
+        Solver s = new Solver();
+        s.Validate(new KakuroBoard(grid3x3));
+    }
+}

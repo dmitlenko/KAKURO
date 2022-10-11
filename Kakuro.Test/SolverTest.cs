ae71d6efc244735038dac91600996151fbf25f50
Denis Mitlenko <softonka@gmail.com>
2022-10-11 20:54:00 +0300

    Fixed wrong values

--- Kakuro.Test/SolverTest.cs
+++ Kakuro.Test/SolverTest.cs
@@ -26,18 +26,18 @@ public class SolverTest
     Cell[,] grid13x13 = new Cell[,]
     {
         {new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(-1,16),new SumCell(-1,3),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell()},
-        {new BlackCell(),new SumCell(-1,16),new SumCell(-1,21),new BlackCell(),new SumCell(11,-1),new WhiteCell(),new WhiteCell(),new SumCell(-1,28),new BlackCell(),new SumCell(-1,29),new SumCell(-1,17),new BlackCell(),new BlackCell()},
-        {new SumCell(4,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(12,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(14,7),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell() },
-        {new SumCell(7,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(-1,16),new SumCell(-1,4),new SumCell(26,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell()},
+        {new BlackCell(),new SumCell(-1,6),new SumCell(-1,21),new BlackCell(),new SumCell(11,-1),new WhiteCell(),new WhiteCell(),new SumCell(-1,28),new BlackCell(),new SumCell(-1,29),new SumCell(-1,17),new BlackCell(),new BlackCell()},
+        {new SumCell(4,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(12, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(14,7),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell() },
+        {new SumCell(7,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(-1, 16),new SumCell(-1, 4),new SumCell(26, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell()},
         {new SumCell(3,-1),new WhiteCell(),new WhiteCell(),new SumCell(12,23),new WhiteCell(),new WhiteCell(),new SumCell(11,29),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell(),new BlackCell() },
         {new BlackCell(),new SumCell(41,16),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,7),new SumCell(-1,38),new SumCell(-1,3)},
-        {new SumCell(17,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(15,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(6,-1),new WhiteCell(),new WhiteCell(),new WhiteCell() },
-        {new SumCell(21,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,29),new SumCell(8,23),new WhiteCell(),new WhiteCell(),new SumCell(-1,16),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell() },
-        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(39,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,6)},
-        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(8,-1),new WhiteCell(),new WhiteCell(),new SumCell(6,-1),new WhiteCell(),new WhiteCell()},
-        {new BlackCell(),new BlackCell(),new SumCell(20,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,17),new SumCell(-1,16),new BlackCell(),new SumCell(9,-1),new WhiteCell(),new WhiteCell() },
-        {new BlackCell(),new BlackCell(),new SumCell(12,-1),new WhiteCell(),new WhiteCell(),new SumCell(21,-1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(10,-1),new WhiteCell(),new WhiteCell() },
-        {new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(17,-1),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell() }
+        {new SumCell(17, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(15, -1),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(6, -1),new WhiteCell(),new WhiteCell(),new WhiteCell() },
+        {new SumCell(21, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1, 29),new SumCell(8, 23),new WhiteCell(),new WhiteCell(),new SumCell(-1,16),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(39, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1,6)},
+        {new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(14,4),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(8, -1),new WhiteCell(),new WhiteCell(),new SumCell(6, -1),new WhiteCell(),new WhiteCell()},
+        {new BlackCell(),new BlackCell(),new SumCell(20, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new WhiteCell(),new SumCell(-1, 17),new SumCell(-1, 16),new BlackCell(),new SumCell(9, -1),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new SumCell(12, -1),new WhiteCell(),new WhiteCell(),new SumCell(21, -1),new WhiteCell(),new WhiteCell(),new WhiteCell(),new BlackCell(),new SumCell(10, -1),new WhiteCell(),new WhiteCell() },
+        {new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell(),new SumCell(17, -1),new WhiteCell(),new WhiteCell(),new BlackCell(),new BlackCell(),new BlackCell(),new BlackCell() }
     };
 
     [TestMethod]
@@ -56,10 +56,9 @@ public class SolverTest
     }
 
     [TestMethod]
-    [ExpectedException(typeof(KakuroException))]
     public void Test13x13()
     {
         Solver s = new Solver();
-        s.Validate(new KakuroBoard(grid3x3));
+        Assert.IsTrue(s.Validate(new KakuroBoard(grid13x13)));
     }
 }

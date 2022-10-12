dc13d7b62be82913b99721c470b003e55e55b0b4
Denis Mitlenko <softonka@gmail.com>
2022-10-12 14:36:16 +0300

    Removed some tests

--- Kakuro.Test/GeneratorTest.cs
+++ Kakuro.Test/GeneratorTest.cs
@@ -10,7 +10,7 @@ namespace Kakuro.Test;
 public class GeneratorTest
 {
     [TestMethod]
-    public void Test4x4Easy()
+    public void Test1()
     {
         Solver s = new Solver();
         Generator g = new Generator();
@@ -23,106 +23,28 @@ public class GeneratorTest
     }
 
     [TestMethod]
-    public void Test4x4Medium()
+    public void Test2()
     {
         Solver s = new Solver();
         Generator g = new Generator();
-        KakuroBoard b = g.Generate(4, 4, 2);
+        KakuroBoard b = g.Generate(6, 6, 2);
 
         Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 4);
-        Assert.AreEqual(b.Height, 4);
+        Assert.AreEqual(b.Width, 6);
+        Assert.AreEqual(b.Height, 6);
         Assert.AreEqual(b.Difficulty, 2);
     }
 
     [TestMethod]
-    public void Test4x4Hard()
+    public void Test3()
     {
         Solver s = new Solver();
         Generator g = new Generator();
-        KakuroBoard b = g.Generate(4, 4, 3);
+        KakuroBoard b = g.Generate(6, 5, 2);
 
         Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 4);
-        Assert.AreEqual(b.Height, 4);
-        Assert.AreEqual(b.Difficulty, 3);
-    }
-
-    [TestMethod]
-    public void Test8x8Easy()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(8, 8, 1);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 8);
-        Assert.AreEqual(b.Height, 8);
-        Assert.AreEqual(b.Difficulty, 1);
-    }
-
-    [TestMethod]
-    public void Test8x8Medium()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(8, 8, 2);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 8);
-        Assert.AreEqual(b.Height, 8);
+        Assert.AreEqual(b.Width, 6);
+        Assert.AreEqual(b.Height, 5);
         Assert.AreEqual(b.Difficulty, 2);
-    }
-
-    [TestMethod]
-    public void Test8x8Hard()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(8, 8, 3);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 8);
-        Assert.AreEqual(b.Height, 8);
-        Assert.AreEqual(b.Difficulty, 3);
-    }
-
-    [TestMethod]
-    public void Test16x16Easy()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(16, 16, 1);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 16);
-        Assert.AreEqual(b.Height, 16);
-        Assert.AreEqual(b.Difficulty, 1);
-    }
-
-    [TestMethod]
-    public void Test16x16Medium()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(16, 16, 2);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 16);
-        Assert.AreEqual(b.Height, 16);
-        Assert.AreEqual(b.Difficulty, 2);
-    }
-
-    [TestMethod]
-    public void Test16x16Hard()
-    {
-        Solver s = new Solver();
-        Generator g = new Generator();
-        KakuroBoard b = g.Generate(16, 16, 3);
-
-        Assert.IsTrue(s.Validate(b));
-        Assert.AreEqual(b.Width, 16);
-        Assert.AreEqual(b.Height, 16);
-        Assert.AreEqual(b.Difficulty, 3);
     }
 }

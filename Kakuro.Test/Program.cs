120ba44eeaa2ba964651d0ce61cfb4d345276838
Denis Mitlenko <softonka@gmail.com>
2022-10-12 14:36:44 +0300

    Update Kakuro.Test

--- Kakuro.Test/Program.cs
+++ Kakuro.Test/Program.cs
@@ -1,65 +1,34 @@
 ﻿using Kakuro.Engine.Algorithms;
-using Kakuro.Engine.Core;
 using Kakuro.Engine.Cells;
+using Kakuro.Engine.Core;
 
-class Program
-{
-    public static void Main()
+class Program{
+    static void Print(KakuroBoard b)
     {
-        Generator generator = new Generator();
-
-        int w = 8;
-        int h = 8;
-        int d = 1;
-
-        // Check validator
-        KakuroBoard b = new KakuroBoard(5, 5);
-        Solver s = new Solver();
-
-        b.WhiteCells = 12;
-        b.Solution = new Dictionary<string, int>(b.WhiteCells);
-        b.Grid = new Cell[,]
+        for(int i = 0; i < b.Height; i++)
         {
-            { new BlackCell(), new BlackCell(), new BlackCell(), new SumCell(-1,24), new SumCell(-1,7) },
-            { new BlackCell(), new SumCell(-1,6), new SumCell(8,24), new WhiteCell(), new WhiteCell() },
-            { new SumCell(18,-1), new WhiteCell(), new WhiteCell(), new WhiteCell(), new WhiteCell() },
-            { new SumCell(23,-1), new WhiteCell(), new WhiteCell(), new WhiteCell(), new WhiteCell() },
-            { new SumCell(12,-1), new WhiteCell(), new WhiteCell(), new BlackCell(), new BlackCell() }
-        };
-
-        b.Solution = s.Solution(b);
-
-        if (s.Validate(b)) Console.WriteLine("valid kakuro");
-        else Console.WriteLine("not a valid kakuro");
-
-        try
-        {
-            KakuroBoard board = generator.Generate(w, h, d);
-
-            if (s.Validate(board)) Console.WriteLine("valid kakuro");
-            else Console.WriteLine("not a valid kakuro");
-
-            for (int i = 0; i < h; i++)
+            for (int j = 0; j < b.Width; j++)
             {
-                for (int j = 0; j < w; j++)
-                {
-                    string o = "";
-                    if (board[i, j] is BlackCell)
-                        o = "";
-                    else if (board[i, j] is SumCell)
-                        o = (board[i, j] as SumCell).ColSum + "\\" + (board[i, j] as SumCell).RowSum;
-                    else if (board[i, j] is WhiteCell)
-                        o = (board[i, j] as WhiteCell).Value.ToString();
+                string o = "";
+                if (b.Grid[i, j] is BlackCell)
+                    o = "";
+                else if (b.Grid[i, j] is SumCell)
+                    o = (b.Grid[i, j] as SumCell).ColSum + "\\" + (b.Grid[i, j] as SumCell).RowSum;
+                else if (b.Grid[i, j] is WhiteCell)
+                    o = (b.Grid[i, j] as WhiteCell).Value.ToString();
 
-                    Console.Write(String.Format("{0,8}", o));
-                }
-                Console.WriteLine();
+                Console.Write(String.Format("{0,8}", o));
             }
-
-        }
-        catch (KakuroException e)
-        {
-            Console.WriteLine(e.Message);
+            Console.WriteLine();
         }
     }
-} 
+
+    public static void Main()
+    {
+        Solver s = new Solver();
+        Generator g = new Generator();
+        KakuroBoard b = g.Generate(6, 6, 2);
+
+        Print(b);
+    }
+}

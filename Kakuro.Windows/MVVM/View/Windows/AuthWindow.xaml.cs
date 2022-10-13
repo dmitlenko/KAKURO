ce475250002e39f9d38fcfa97d93d4dfaaddb42c
Denis Mitlenko <softonka@gmail.com>
2022-10-13 20:39:23 +0300

    Project reorganization

--- Kakuro.Windows/MVVM/View/Windows/AuthWindow.xaml.cs
+++ Kakuro.Windows/MVVM/View/Windows/AuthWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Kakuro.Engine.Authorization;
+using Kakuro.Engine.Rankings;
 
 
 namespace Kakuro.Windows
@@ -24,6 +25,16 @@ namespace Kakuro.Windows
         public AuthWindow()
         {
             InitializeComponent();
+
+            SerealizedRanksList ranker = new SerealizedRanksList();
+
+            /*ranker.Add(new UserRank(0, 10, 10, 10));
+            ranker.Add(new UserRank(1, 10, 10, 10));
+            ranker.Add(new UserRank(2, 10, 10, 10));*/
+
+            for (int i = 0; i < ranker.Count; MessageBox.Show(ranker[i++].TimeString())) ;
+
+            ranker.Save();
         }
     }
 }

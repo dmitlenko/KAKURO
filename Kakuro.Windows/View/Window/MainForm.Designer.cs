21a4560da38dc0908f49d6f0413cf84b551ee6c3
Denis Mitlenko <softonka@gmail.com>
2022-10-25 14:16:56 +0300

    Update MainForm.Designer.cs

--- Kakuro.Windows/View/Window/MainForm.Designer.cs
+++ Kakuro.Windows/View/Window/MainForm.Designer.cs
@@ -117,6 +117,7 @@
             this.toolStripButton6});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(12, 0, 12, 0);
             this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.toolStrip1.Size = new System.Drawing.Size(593, 27);
             this.toolStrip1.TabIndex = 1;

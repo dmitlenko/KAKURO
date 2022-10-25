1456d57482c9233d80dc66e7d68007cab7c012ab
Denis Mitlenko <softonka@gmail.com>
2022-10-25 13:47:21 +0300

    Added some resources

--- Kakuro.Windows/Properties/Resources.Designer.cs
+++ Kakuro.Windows/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Kakuro.Windows.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap flag_flyaway_pointed1 {
+            get {
+                object obj = ResourceManager.GetObject("flag_flyaway_pointed1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap folder_vertical_open {
             get {
                 object obj = ResourceManager.GetObject("folder_vertical_open", resourceCulture);
